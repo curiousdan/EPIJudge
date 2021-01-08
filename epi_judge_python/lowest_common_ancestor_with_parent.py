6e54a8704d882e4b90b54457faf433163507d684
Dan <curiousdan@users.noreply.github.com>
2021-01-07 23:37:05 -0600

    Finish to 9.5
    Jan 7 2021. 9.5 should be redone

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -7,26 +7,109 @@ from test_framework.binary_tree_utils import must_find_node
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+# reimplemntation of the book answer
+# 1/7/21
+# 9.4
+def lca(node0, node1):
+	def depth(node):
+		ret = 0
+		while node:
+			ret += 1
+			node = node.parent
+		return ret
+	
+	depth0, depth1 = map(depth, (node0, node1))
+	if depth1 > depth0: # force node 0 to be the deeper node
+		node0, node1 = node1, node0
+		
+	delta = abs(depth1 - depth0)
+	for i in range(delta):
+		node0 = node0.parent
+	
+	while node0 != node1:
+		node0, node1 = node0.parent, node1.parent
+	return node0	
 
-def lca(node0: BinaryTreeNode,
+
+
+def lca_book(node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    def get_depth(node):
+        depth = 0
+        while node.parent:
+            depth += 1
+            node = node.parent
+        return depth
+
+    depth0, depth1 = map(get_depth, (node0, node1))
+    # Makes node0 as the deeper node in order to simplify the code.
+    if depth1 > depth0:
+        node0, node1 = node1, node0
+
+    # Ascends from the deeper node.
+    depth_diff = abs(depth0 - depth1)
+    while depth_diff:
+        node0 = node0.parent
+        depth_diff -= 1
+
+    # Now ascends both nodes until we reach the LCA.
+    while node0 is not node1:
+        node0, node1 = node0.parent, node1.parent
+    return node0
+
+
+# below is my failed attempt. It failed because namedtuples are immutable lmao
+from collections import  namedtuple
+def my_failed_lca(node0: BinaryTreeNode,
+								node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+	nodeAndPath = namedtuple("nodeAndPath",("n","path"))
+	n0 = nodeAndPath(node0, set())
+	n1 = nodeAndPath(node1, set())
+	
+	def check_and_advance(advancing, static):
+		if advancing.n in static.path:
+			print("is")
+			ret = advancing.n
+			advancing.n = None
+			static.n = None
+			return ret
+		else:
+			print("else")
+			advancing.path.add(advancing.n)
+			print("next")
+			advancing.n = advancing.n.parent			
+			print("done")
+		
+		
+	while n0.n and n1.n:
+		print("iter")
+		if n0.n:
+			print("n0")
+			r = check_and_advance(n0,n1)
+			if r:
+				return r
+		if n1.n:
+			print("n1")
+			r = check_and_advance(n1,n0)
+			if r:
+				return r
+			
+	return None
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, node0, node1):
-    result = executor.run(
-        functools.partial(lca, must_find_node(tree, node0),
-                          must_find_node(tree, node1)))
+	result = executor.run(
+		functools.partial(lca,
+																				must_find_node(tree, node0), must_find_node(tree, node1)))
 
-    if result is None:
-        raise TestFailure('Result can\'t be None')
-    return result.data
+	if result is None:
+		raise TestFailure('Result can\'t be None')
+	return result.data
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('lowest_common_ancestor_with_parent.py',
-                                       'lowest_common_ancestor.tsv',
-                                       lca_wrapper))
+	exit(
+		generic_test.generic_test_main('lowest_common_ancestor_with_parent.py',
+																																	'lowest_common_ancestor.tsv', lca_wrapper))
+	
--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -2,13 +2,70 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+# 9.5
+# 1/7/21
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+	# the insight is that bit shift left is eequal to 2*number
+	def helper(tree, partial_sum):
+		if not tree:
+			return 0
+		partial_sum = (partial_sum << 1) + tree.data
+		
+		# necessary, because leaf MUST return the sum at that point
+		if not tree.left and not tree.right:
+			return partial_sum
+
+		# non-leaves just return the sum of its returned values. 
+		l = r= 0
+		if tree.left:
+			l = helper(tree.left, partial_sum)
+		if tree.right:
+			r = helper(tree.right, partial_sum)
+		return l + r
+	return helper(tree, 0)
+					
+		
+
+# book soln. wow very clever. Actually faster than reimplementation above. 
+# seems like if statements take longer than simple recursion.
+def book_sum_root_to_leaf(tree: BinaryTreeNode) -> int:
+    def sum_root_to_leaf_helper(tree, partial_path_sum=0):
+        if not tree:
+            return 0
+
+        partial_path_sum = partial_path_sum *2 + tree.data
+        if not tree.left and not tree.right:  # Leaf.
+            return partial_path_sum
+        # Non-leaf.
+        return (sum_root_to_leaf_helper(tree.left, partial_path_sum) +
+                sum_root_to_leaf_helper(tree.right, partial_path_sum))
+
+    return sum_root_to_leaf_helper(tree)
+
+from functools import reduce	
+def attempt1_sum_root_to_leaf(tree: BinaryTreeNode) -> int:
+	if not tree:
+		return 0
+		
+	def build_paths(node):
+		l = r = []
+		if node.left:	
+			l = build_paths(node.left)
+		if node.right:
+			r = build_paths(node.right)
+		combined = l + r
+		if not combined:
+			return [str(node.data)]
+		return [str(node.data) + element for element in combined]
+		
+	all_paths = build_paths(tree)
+	all_paths[0] = int(all_paths[0],2)
+	int_and_sum = lambda x, y: x+int(y,2)
+	return reduce(int_and_sum, all_paths)
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('sum_root_to_leaf.py',
-                                       'sum_root_to_leaf.tsv',
-                                       sum_root_to_leaf))
+	exit(
+		generic_test.generic_test_main('sum_root_to_leaf.py', 'sum_root_to_leaf.tsv',
+																																	sum_root_to_leaf))
+
